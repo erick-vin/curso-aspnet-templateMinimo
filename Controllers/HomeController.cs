6d978064eaff03699f0c7b1046953b6c8a5b0404
erick.santos3 <erick.santos@segfy.com>
2022-05-06 17:58:10 -0300

    Criando aplicação web partindo de um template minimo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsandoViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View("FormUsuario");
+        }
+        
+    }
+}
